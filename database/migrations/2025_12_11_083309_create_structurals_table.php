f1464748bf2a84655faf3c4e519c71f7c5d42242
rksaragih <168797432+rksaragih@users.noreply.github.com>
2026-05-10 15:08:35 +0700

    Migration Cleaning

--- database/migrations/2025_12_11_083309_create_structurals_table.php
+++ database/migrations/2025_12_11_083309_create_structurals_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('jabatan');
             $table->string('image');
             $table->string('caption');
-            $table->text('deskripsi')->nullable();
             $table->timestamps();
         });
     }
